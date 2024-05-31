07842b967afccdf2ec425252140303c15fcf413b
Pavel Ogurtsov <cucumb3rpasha@gmail.com>
2024-05-31 17:57:28 +0300

    gems

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
-    set -gx PATH ~/bin ~/go/bin $PATH
-    # Commands to run in interactive sessions can go here
+    set -gx PATH ~/bin ~/go/bin $HOME/gems/bin $PATH
+    
+    set -gx GEM_HOME $HOME/gems
+
+
+    eval (ssh-agent -c) > /dev/null
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
